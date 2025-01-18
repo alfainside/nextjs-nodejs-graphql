1b07b2813ea57ab38628e9aacb792e4e20652410
lutfialfa.dev <lutfialfa.dev@gmail.com>
2025-01-18 17:35:45 +0700

    init

--- maintenance-requests-backend/src/schema.ts
+++
@@ -1,8 +0,0 @@
-import { buildSchema } from "type-graphql";
-import { MaintenanceRequestResolver } from "./resolvers/MaintenanceRequestResolver";
-
-export const createSchema = async () => {
-  return await buildSchema({
-    resolvers: [MaintenanceRequestResolver],
-  });
-};
